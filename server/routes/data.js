cb5e78b2d057309ccfa9ea67fc780b3f71bb1dac
johnk928313 <61567448+kimjon0509@users.noreply.github.com>
2021-05-14 20:05:29 -0400

    cleaned data to be used for data visulisation

--- server/routes/data.js
+++ server/routes/data.js
@@ -31,6 +31,46 @@ app.get("/reported_raw",(req, res) => {
 
   });
 
+app.get("/outbreak_raw",(req, res) => {
+
+  db.query(`
+  SELECT * 
+  FROM outbreak_raw
+  `)
+  .then(data => {
+    res.send(data.rows)
+  })
+
+});
+
+app.get("/figure1", (req, res) => {
+  db.query(`
+  SELECT reported_date,
+       UNNEST(ARRAY ['recovered', 'active', 'deceased']) AS key,
+       UNNEST(ARRAY [recovered, active, deceased]) AS count
+  FROM reported_raw
+  `)
+  .then(data => {
+    res.send(data.rows)
+  })
+});
+
+app.get("/figure2",(req, res) => {
+
+  db.query(`
+  SELECT episode_week, outbreak_or_sporadic ,
+  SUM(cases) AS total_cases
+  FROM outbreak_raw
+  GROUP BY 
+    episode_week,
+    outbreak_or_sporadic
+  ORDER BY episode_week ASC
+  `)
+  .then(data => {
+    res.send(data.rows)
+  })
+
+});
 
 server.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
